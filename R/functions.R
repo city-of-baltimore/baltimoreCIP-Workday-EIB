74881776214a6344cc1dfc40888a777f735cd240
your name <eli.pousson@gmail.com>
2024-06-27 01:08:43 -0400

    Split off validation + filter functions
    Also use pick to get arrange working with column name vector

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,35 @@
+validate_project_dates <- function(data,
+                                   curr_fy_start_date) {
+  data |>
+    dplyr::mutate(
+      "Project Start Date Flag" := case_when(
+        `Project Start Date` > curr_fy_start_date ~ "Start date after current fiscal year",
+        `Project Start Date` < lubridate::date("2022-07-01") ~ "Start date before FY23 fiscal year",
+        is.na(`Project Start Date`) ~ "Missing start date"
+      ),
+      "Project End Date Flag" := case_when(
+        is.na(`Project End Date`) ~ "Missing end date",
+        `Project End Date` < curr_fy_start_date ~ "End date before current fiscal year",
+        `Project End Date` == curr_fy_start_date ~ "End date set to first day of current fiscal year"
+      )
+    )
+}
+
+validate_budget_plan_dates <- function(data,
+                                       curr_fy_start_date = NULL) {
+  curr_fy_start_date <- lubridate::as_date(curr_fy_start_date)
+
+  data |>
+    dplyr::mutate(
+      "Date From Flag" := case_when(
+        is.na(`Date From`) & is.na(`Plan Status`) ~ "Missing Date From",
+        `Date From` < curr_fy_start_date ~ "Date From before current fiscal year",
+        `Date From` > `Project Start Date` ~ "Date From after Project Start Date",
+        `Date From` != `Project Start Date` ~ "Date From mismatch with Project Start Date"
+      )
+    )
+}
+
 #' Format a pair of code and name columns from a Workday or Adaptive Planning report
 fmt_wd_code_name <- function(data,
                              code_col,
@@ -32,7 +64,7 @@ fmt_cip_data <- function(
     fmt_wd_code_name(
       "Cost Center Code",
       "Cost Center Name",
-      "^(CAP|RES)[:digit:]+"
+      "^(CAP|RES|zzDNU_CAP|DNU_CAP)[:digit:]+"
     ) |>
     fmt_wd_code_name(
       "Project Code",
@@ -56,6 +88,32 @@ fmt_cip_data <- function(
 }
 
 
+cbind_default_cols <- function(data, default_cols) {
+  if (!is.data.frame(default_cols)) {
+    default_cols <- tibble::as_tibble(default_cols)
+  }
+
+  purrr::list_cbind(
+    list(
+      data,
+      default_cols
+    )
+  )
+}
+
+#' Format a header key based on the project code
+fmt_header_key <- function(data,
+                           project_code_col = "Project Code",
+                           header_key_col = "Header Key") {
+  data |>
+    dplyr::mutate(
+      "{header_key_col}" := cur_group_id(),
+      .by = all_of(project_code_col),
+      .before = all_of(project_code_col)
+    )
+}
+
+#' Format EIB budget lines data
 fmt_eib_budget <- function(
     data,
     project_code_col = "Project Code",
@@ -100,7 +158,61 @@ fmt_eib_budget <- function(
     dplyr::arrange({{ project_code_col }})
 }
 
+filter_project_end_date <- function(data) {
+  project_missing_end_date <- data |>
+    filter(
+      is.na(.data[["Project End Date"]])
+    ) |>
+    distinct(`Project Code`)
 
+  n_missing_end_date <- nrow(project_missing_end_date)
+
+  if (n_missing_end_date > 0) {
+    cli::cli_bullets(
+      c(
+        "Excluding {n_missing_end_date} project{?s} with with a missing end date.",
+        "i" = "Projects with missing end dates can't be loaded with a Put Budget EIB."
+      )
+    )
+
+    data <- data |>
+      dplyr::filter(
+        !is.na(.data[["Project End Date"]])
+      )
+  }
+
+  data
+}
+
+filter_project_start_date <- function(data) {
+  project_late_start_date <- data |>
+    filter(
+      .data[["Project Start Date"]] > lubridate::date("2024-07-01 UTC")
+    ) |>
+    distinct(`Project Code`)
+
+  n_late_start_date <- nrow(project_late_start_date)
+
+  # FIXME: Add date checks
+  if (n_late_start_date > 0) {
+    cli::cli_bullets(
+      c(
+        "Excluding {n_late_start_date} project{?s} with with a late start date.",
+        "i" = "Projects with start dates after the current fiscal year should
+        be corrected before creating a budget plan."
+      )
+    )
+
+    data <- data |>
+      dplyr::filter(
+        .data[["Project Start Date"]] <= lubridate::date("2024-07-01 UTC")
+      )
+  }
+
+  data
+}
+
+#' Build a put budget EIB
 build_put_budget_eib <- function(data,
                                  project_code_col = "Project Code",
                                  cols = c(
@@ -108,7 +220,7 @@ build_put_budget_eib <- function(data,
                                    "Project" = project_code_col
                                  ),
                                  amt_col = getOption("curr_fy_col", "FY2025"),
-                                 .defaults = list(
+                                 plan_defaults = list(
                                    "Add Only" = "Y",
                                    "Company+" = "COB",
                                    "Plan Structure*" = "Capital_Project_Budget",
@@ -127,51 +239,18 @@ build_put_budget_eib <- function(data,
       .data[[amt_col]] > 0
     )
 
-  if (any(is.na(data[["Project End Date"]]))) {
-    cli::cli_warn(
-      "Budget Plan can't be loaded for any projects with a missing end date."
-    )
-
-    data <- data |>
-      dplyr::filter(
-        !is.na(.data[["Project End Date"]])
-      )
-  }
-
-  # FIXME: Add date checks
-  if (any(data[["Project Start Date"]] > lubridate::date("2024-07-01 UTC"))) {
-    cli::cli_warn(
-      "Budget Plan can't be loaded for any projects with a missing end date."
-    )
-
-    data <- data |>
-      dplyr::filter(
-        !is.na(.data[["Project End Date"]])
-      )
-  }
-
   data |>
+    filter_project_end_date() |>
+    # filter_project_start_date() |>
     dplyr::distinct(pick(all_of(project_code_col)), .keep_all = TRUE) |>
     dplyr::select(all_of(cols)) |>
     dplyr::mutate(
       "Spreadsheet Key*" := row_number()
     ) |>
-    dplyr::bind_cols(
-      tibble::as_tibble(.defaults)
-    )
+    cbind_default_cols(plan_defaults)
 }
 
-fmt_header_key <- function(data,
-                           project_code_col = "Project Code",
-                           header_key_col = "Header Key") {
-  data |>
-    dplyr::mutate(
-      "{header_key_col}" := cur_group_id(),
-      .by = all_of(project_code_col),
-      .before = all_of(project_code_col)
-    )
-}
-
+#' Build a budget ammendment EIB
 build_ammend_budget_eib <- function(data,
                                     budget_cols = c(
                                       "Budget Name" = "Project Name",
@@ -185,6 +264,7 @@ build_ammend_budget_eib <- function(data,
                                       "Revenue Category" = "Revenue Category Code",
                                       header_key_col,
                                       line_key_col,
+                                      ledger_account_col,
                                       debit_col,
                                       credit_col
                                     ),
@@ -207,13 +287,21 @@ build_ammend_budget_eib <- function(data,
                                     amt_col = getOption("curr_fy_col"),
                                     header_key_col = "Header Key",
                                     line_key_col = "Line Key",
+                                    ledger_account_col = "Ledger Account Summary",
                                     debit_col = "Budget Debit Amount",
                                     credit_col = "Budget Credit Amount") {
+  # Filter to positive amount values only
   data <- data |>
     dplyr::filter(
       .data[[amt_col]] > 0
     )
 
+  # Filter invalid project start and end date values
+  data <- data |>
+    filter_project_end_date() |>
+    filter_project_start_date()
+
+  # Create first sheet (budget data)
   budget_data <- data |>
     fmt_header_key(
       project_code_col = project_code_col,
@@ -223,14 +311,15 @@ build_ammend_budget_eib <- function(data,
       all_of(budget_cols)
     ) |>
     dplyr::distinct() |>
-    dplyr::bind_cols(
-      tibble::as_tibble(budget_defaults)
-    ) |>
+    cbind_default_cols(budget_defaults) |>
     dplyr::arrange(
       # FIXME: Hard-coded header key value
-      `Header Key*`
+      pick(
+        all_of("Header Key*")
+      )
     )
 
+  # Create second sheet (entry data)
   entry_data <- data |>
     fmt_eib_budget(
       project_code_col = project_code_col,
@@ -243,15 +332,11 @@ build_ammend_budget_eib <- function(data,
     dplyr::select(
       all_of(entry_cols)
     ) |>
-    dplyr::bind_cols(
-      tibble::as_tibble(entry_defaults)
-    ) |>
+    cbind_default_cols(entry_defaults) |>
     dplyr::arrange(
-      # FIXME: Hard-coded header and line key value
-      `Header Key`,
-      `Line Key`
-      # {{ header_key_col }},
-      # {{ line_key_col }}
+      pick(
+        all_of(c(header_key_col, line_key_col))
+      )
     )
 
   list(
@@ -303,8 +388,6 @@ build_new_budget_eib <- function(data,
     )
 
   budget_data_keys <- data |>
-    # FIXME: Correct hard-coded project code ID
-    # dplyr::distinct(`Project Code`, .keep_all = TRUE) |>
     dplyr::distinct(pick(all_of(project_code_col)), .keep_all = TRUE) |>
     fmt_header_key(
       project_code_col = project_code_col,
@@ -315,9 +398,7 @@ build_new_budget_eib <- function(data,
     dplyr::select(
       all_of(budget_cols)
     ) |>
-    dplyr::bind_cols(
-      tibble::as_tibble(budget_defaults)
-    )
+    cbind_default_cols(budget_defaults)
 
   budget_lines_data <- data |>
     fmt_eib_budget(
@@ -335,21 +416,26 @@ build_new_budget_eib <- function(data,
     dplyr::left_join(
       budget_data_keys |>
         dplyr::select(
-          all_of(c(header_key_col,
-            "Year" = "Fiscal Year Start"
-          ))
+          all_of(
+            c(
+              header_key_col,
+              "Year" = "Fiscal Year Start"
+            )
+          )
         ),
+      relationship = "many-to-one",
       by = header_key_col
     ) |>
-    dplyr::bind_cols(
-      tibble::as_tibble(line_defaults)
-    ) |>
+    cbind_default_cols(line_defaults) |>
     dplyr::mutate(
       "{line_order_col}" := .data[[line_key_col]],
       .after = all_of(line_key_col)
     ) |>
-    # FIXME: Correct hard-coded column names
-    dplyr::arrange(`Header Key`, `Line Key`)
+    dplyr::arrange(
+      pick(
+        all_of(c(header_key_col, line_key_col))
+      )
+    )
 
   list(
     "Import Budget High Volume" = budget_data,
--- _targets.R
+++ _targets.R
@@ -6,7 +6,8 @@ library(tidyverse)
 # Set target options:
 tar_option_set(
   tidy_eval = TRUE,
-  packages = c("tidyverse", "openxlsx2"),
+  # pak::pkg_install("elipousson/sharepointr"),
+  packages = c("tidyverse", "openxlsx2", "here", "readxl"),
   format = "qs"
 )
 
@@ -14,7 +15,10 @@ tar_source()
 
 options(
   cip_data_filename = "Capital_Projects_-_Six-Year_CIP.xlsx",
-  plan_info_filename = "Capital Projects with Plan Info 6.17.24.xlsx",
+  plan_info_filename = "Capital_Projects_With_Plan_Info 2024-06-24.xlsx",
+  plan_info_skip = 1,
+  # plan_info_filename = "Capital_Projects_With_Plan_Info_Sandbox-2024-06-21.xlsx",
+  # plan_info_skip = 3,
   invalid_date_report_filename = "2024-06-17_CIP-Projects_Invalid-Dates.xlsx",
   curr_fy_col = "FY2025",
   curr_fy = 2024,
@@ -26,32 +30,7 @@ options(
 
 # Replace the target list below with your own:
 tar_plan(
-
-  # Get path to report supplied by Kristen Ahearn on 6.4.2024
-  # NOTE: File is not included in repository
-  prj_plan_info_report_path = here::here(
-    "_targets", "user", "data",
-    getOption("plan_info_filename")
-  ),
-
-  # Read Workday report on project with plan info
-  prj_plan_info_report = readxl::read_excel(
-    prj_plan_info_report_path,
-    skip = 1
-  ) |>
-    # Categorize by type
-    dplyr::mutate(
-      put = is.na(`Plan Status`),
-      new = `Plan Status` == "Draft",
-      amend = `Plan Status` == "Available"
-    ),
-
-  # Make a list of project IDs for each type
-  project_eib_type = list(
-    "put" = filter(prj_plan_info_report, put) |> pull(`Project ID`),
-    "new" = filter(prj_plan_info_report, new) |> pull(`Project ID`),
-    "amend" = filter(prj_plan_info_report, amend) |> pull(`Project ID`)
-  ),
+  curr_fy_start_date = lubridate::date(getOption("curr_fy_start_date_str")),
 
   # Set file path for Capital Projects 6-Year CIP report
   # NOTE: File is not included in repository
@@ -60,42 +39,75 @@ tar_plan(
     getOption("cip_data_filename")
   ),
 
-  curr_fy_start_date = lubridate::date(getOption("curr_fy_start_date_str")),
-
   # Read CIP data from file
-  cip_data = readxl::read_excel(cip_data_path) |>
-    filter(
-      `Cost Center Code` != "Total"
+  cip_data_src = readxl::read_excel(cip_data_path) |>
+    dplyr::filter(`Cost Center Code` != "Total"),
+
+  # Get path to report supplied by Kristen Ahearn
+  # NOTE: File is not included in repository
+  prj_plan_info_report_path = here::here(
+    "_targets", "user", "data",
+    getOption("plan_info_filename")
+  ),
+
+  # Read Workday report on project with plan info
+  prj_plan_info_report_src = readxl::read_excel(
+    path = prj_plan_info_report_path,
+    skip = getOption("plan_info_skip")
+  ),
+  prj_plan_info_report = prj_plan_info_report_src |>
+    dplyr::filter(
+      # Exclude denied Plan Status information
+      is.na(`Plan Status`) | `Plan Status` != "Denied",
+      .data[["Project ID"]] %in% cip_data_src[["Project Code"]]
     ) |>
+    mutate(
+      # Flag duplicate budget plan information
+      budget_plan_id = row_number(),
+      .by = `Project ID`
+    ) |>
+    filter(
+      # Keep only the first budget plan if multiple exist
+      budget_plan_id == 1
+    ) |>
+    mutate(
+      `Project Start Date` = `Project Start Date`,
+      `Project End Date` = `Project End Date`,
+      `Project Code` = `Project ID`,
+      `Date From`,
+      `Date To`,
+      `Plan Status`,
+      # NOTE: Fiscal year start is based on date from of the plan period
+      # Not the Project start date
+      `Fiscal Year Start` = year(`Date From`) + (month(`Date From`) >= 7),
+      # Categorize by type
+      put = is.na(`Plan Status`),
+      # TODO: Document how the plan status is assigned
+      new = is.na(`Plan Status`) | `Plan Status` %in% c("Draft"),
+      amend = `Plan Status` %in% c("Available", "In Progress"),
+      .keep = "none"
+    ),
+
+  # Make a list of project IDs for each type
+  project_eib_type = list(
+    "put" = filter(prj_plan_info_report, put) |> pull(`Project Code`),
+    "new" = filter(prj_plan_info_report, new) |> pull(`Project Code`),
+    "amend" = filter(prj_plan_info_report, amend) |> pull(`Project Code`)
+  ),
+  cip_data = cip_data_src |>
     fmt_cip_data() |>
     summarise_cip_data_fy(
       .by = c(
-        "FGSFund Code",
-        "FGSFund Name",
-        "Cost Center Code",
-        "Cost Center Name",
-        "Revenue Category Code",
-        "Revenue Category Name",
-        "Grant_Detail Name",
-        "Project Code",
-        "Project Name"
+        "FGSFund Code", "FGSFund Name",
+        "Cost Center Code", "Cost Center Name",
+        "Revenue Category Code", "Revenue Category Name",
+        "Grant_Detail Name", "Project Code", "Project Name"
       )
     ) |>
     dplyr::left_join(
       # Join a Fiscal Year column inferred from the Date From column
-      prj_plan_info_report |>
-        mutate(
-          `Project Start Date` = `Project Start Date`,
-          `Project End Date` = `Project End Date`,
-          `Project Code` = `Project ID`,
-          `Date From`,
-          `Date To`,
-          `Plan Status`,
-          # NOTE: Fiscal year start is based on date from of the plan period
-          # Not the Project start date
-          `Fiscal Year Start` = year(`Date From`) + (month(`Date From`) >= 7),
-          .keep = "none"
-        ),
+      prj_plan_info_report,
+      # relationship = "many-to-one",
       by = "Project Code",
       na_matches = "never"
     ),
@@ -106,23 +118,13 @@ tar_plan(
     distinct(`Project Code`) |>
     pull(`Project Code`),
   cip_data_project_date_validation = cip_data |>
-    dplyr::mutate(
-      "Project Start Date Flag" := case_when(
-        `Project Start Date` > curr_fy_start_date ~ "Start date after current fiscal year",
-        `Project Start Date` < lubridate::date("2022-07-01") ~ "Start date before FY23 fiscal year",
-        is.na(`Project Start Date`) ~ "Missing start date"
-      ),
-      "Project End Date Flag" := case_when(
-        is.na(`Project End Date`) ~ "Missing end date",
-        `Project End Date` < curr_fy_start_date ~ "End date before current fiscal year",
-        `Project End Date` == curr_fy_start_date ~ "End date set to first day of current fiscal year"
-      )
-    ) |>
+    validate_project_dates(curr_fy_start_date) |>
+    validate_budget_plan_dates(curr_fy_start_date) |>
     dplyr::select(
       !c(
         starts_with("FY"),
         starts_with("Revenue"),
-        any_of(c("Plan Status", "Date From", "Date To", "Fiscal Year Start"))
+        any_of(c("Fiscal Year Start"))
       )
     ) |>
     distinct() |>
@@ -130,8 +132,7 @@ tar_plan(
       # FIXME: Hard-coded fiscal year name
       "Has FY2025 Funding" := dplyr::if_else(
         .data[["Project Code"]] %in% cip_curr_fy_projects,
-        "Y",
-        "N"
+        "Y", "N"
       )
     ) |>
     relocate(
@@ -164,11 +165,11 @@ tar_plan(
   # Create new capital projects EIB
   new_budget_eib = cip_data |>
     filter(
-      # FIXME: Made this change from new only to new *and* put on 2024-06
-      .data[["Project Code"]] %in% c(
-        project_eib_type[["put"]],
-        project_eib_type[["new"]]
-      )
+      .data[["Project Code"]] %in% project_eib_type[["new"]]
+    ) |>
+    filter(
+      # FIXME: Temporarily adding filter to account for missing dates in report
+      !is.na(`Fiscal Year Start`)
     ) |>
     build_new_budget_eib(),
 
@@ -193,5 +194,22 @@ tar_plan(
     template = here::here("files", "COB_Import_Project_Budget_Amendment Template.xlsx"),
     file = here::here("_output", "COB_Import_Project_Budget_Amendment.xlsx"),
     overwrite = TRUE
-  )
+  ),
+  curr_fy_cip_data_excluded = cip_data |>
+    dplyr::filter(
+      FY2025 > 0,
+      is.na(`Fiscal Year Start`)
+      # NOTE: Enable the following to filter to records that are included
+      # !(
+      #   `Project Name` %in% c(
+      #     amend_budget_eib[["Import Budget Amendment"]][["Budget Name"]],
+      #     new_budget_eib[["Import Budget High Volume"]][["Budget Name*"]]
+      #   )
+      # )
+    )
+  # TODO: Implement a summary report
+  #   tarchetypes::tar_quarto(
+  #     qto_report,
+  #     here::here("report.qmd")
+  #   )
 )
